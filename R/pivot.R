567f2a7773e48a6042564eb1db88dc38336a4839
A.G <amingassem@gmail.com>
2015-07-16 00:38:29 +0200

    add rpivot doc

--- R/pivot.R
+++ R/pivot.R
@@ -1,11 +1,26 @@
 
 
+#' Create orb pivot widget
+#'
+#' This function creates an orb widget using \pkg{htmlwidgets}. The
+#' widget can be rendered on HTML pages generated from R Markdown, Shiny, or
+#' other applications.
+#'
+
+#' @param data a data object (currently supported objects are matrices, data
+#'   frames, and data.table
+#' @param width the width of the pivot
+#' @param height the height of the pivot
+#' @return A HTML widget object).
+#' @examples inst/examples/pivot.R
+#' @export
+#'
 rpivot = function(data = NULL, width = NULL, height = NULL) {
   htmlwidgets::createWidget(
     'rpivot',
     structure(
       list(),
-      orbDataSource = data
+      dataSource = data
     ),
     width = width, height = height,
     sizingPolicy = htmlwidgets::sizingPolicy(
